138275c4cafa92fcc37611b86ec52bda2b698b6f
Kirill Seredavin <yoghtru@gmail.com>
2020-03-02 19:03:57 +0400

    clear comment

--- src/main/java/org/bot_docmng/Bot.java
+++ src/main/java/org/bot_docmng/Bot.java
@@ -28,9 +28,6 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     static void start() {
-//        System.getProperties().put( "proxySet", "true" );
-//        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
-//        System.getProperties().put( "socksProxyPort", "9150" );
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
